a013b15f71294743e4a2e97a1e8183619081a1ca
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-08 00:59:05 -0300

    1001 - Extremely Basic

---
+++ More Exercises/URI Exercises/1 - Beginner/1001 - Extremely Basic.js
@@ -0,0 +1,16 @@
+// ESTE PROGRAMA RECEBE DUAS VARIÁVEIS E EXIBE A SOMA DE SEUS VALORES.
+
+const readline = require("readline");
+
+const cosulta = readline.createInterface({
+
+    input: process.stdin, output: process.stdout});
+    
+cosulta.on("line", (n1) => {
+
+    cosulta.on("line", (n2) => {
+
+        console.log("X = " + (Number(n1) + Number(n2)))
+        cosulta.close()
+    })
+})
