b5e40d09a9fda56f1dd1fcffcf0ade48011a6c88
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-08 01:07:50 -0300

    1017 - Fuel Expenditure

---
+++ More Exercises/URI Exercises/1 - Beginner/1017 - Fuel Expenditure.js
@@ -0,0 +1,17 @@
+const readline = require("readline")
+
+const consulta = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+consulta.on("line", (horas) => {
+    consulta.on("line", (distancia) => {
+     
+        kmlitro = (horas * distancia) / 12
+
+        console.log(`${kmlitro.toFixed(3)}`)
+
+        consulta.close()
+    })
+})
