0651d658149863a2863b9adbdeef97f4d90df8db
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-08 01:07:25 -0300

    1016 - Distance

---
+++ More Exercises/URI Exercises/1 - Beginner/1016 - Distance.js
@@ -0,0 +1,15 @@
+const readline = require("readline")
+
+const consulta = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+consulta.on("line", (distancia) => {
+    
+        y = distancia * 2
+
+        console.log(`${y} minutos`)
+
+        consulta.close()
+    })
