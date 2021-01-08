6bdd2a81bf9a9d7cc6e01600ab88086a7ea84b9b
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-08 15:28:09 -0300

    Lesson 3

---
+++ TypeScript and JavaScript, basic to advanced/0 - Console.log Presentation/index.js
@@ -0,0 +1,10 @@
+//Types of console.log
+
+console.log("Hello World!" , 7000000000, "S" + 2 );
+
+console.log(" PermaCache is the best 'server!'");
+
+console.log(' PermaCache is the best "server!"');
+
+console.log(`'PermaCache' is the best "server!"`);
+
