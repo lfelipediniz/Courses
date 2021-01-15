9359a9b0bf1fbe3cbaac029638c9aa96b9ec88b5
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-15 16:07:35 -0300

    Using strings in the Console

--- TypeScript and JavaScript/0 - Basic JavaScript/0 - Console.log Presentation/index.js
+++ TypeScript and JavaScript/0 - Basic JavaScript/0 - Console.log Presentation/index.js
@@ -1,10 +1,11 @@
-//Types of console.log
 
 console.log('==================================================')  
 
     console.log("Hello World!" , 7000000000, "S" + 2 );
 
 console.log('==================================================')  
+
+//Types of console.log
 
     console.log(" PermaCache is the best 'server!'");
 
