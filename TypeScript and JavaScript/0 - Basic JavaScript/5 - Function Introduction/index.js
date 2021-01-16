ce6bfff45274fd58aa49101242d482218023aa0b
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-16 16:30:22 -0300

    Function Introduction

---
+++ TypeScript and JavaScript/0 - Basic JavaScript/5 - Function Introduction/index.js
@@ -0,0 +1,36 @@
+//everything that is in the scope of the function, is protected
+
+// Classical Function
+
+    function sum(x, y) {
+
+         result = x + y;
+
+        return result;
+    }
+
+console.log(sum(2, 4));
+
+// Anonymous Function
+
+    const root = function (n) {
+
+        return n ** 0.5;
+    };
+
+console.log(root(9))
+
+// Arrow Function
+
+    const multiplication = (a, b) => {
+
+        return a * b;
+    };
+
+console.log(multiplication(5, 10))
+
+// Arrow function with only one line in the function, and only one parameter
+
+const squared = j => j * j;
+
+console.log(squared(10))
