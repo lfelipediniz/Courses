123cc7a98d1ff1b5dbe103af07e421a1a7604577
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-16 19:29:36 -0300

    Objects Introduction

---
+++ TypeScript and JavaScript/0 - Basic JavaScript/6 - Objects Introduction/index.js
@@ -0,0 +1,38 @@
+// simple object creation
+
+const person1 = {
+
+    name: 'Luiz Felipe',
+    surname: 'Diniz Costa',
+    age: 16
+};
+
+console.log(person1.name)
+
+// using function to return the object, in other words, an object factory
+
+const registerPeople = (name, surname, age) => {
+    return {
+        name, // = name: name, 
+        surname, // = surname: surname,
+        age, // =  age: age,
+    };
+}
+
+const persson1 = registerPeople('Luiz' , 'Costa', 16 );
+console.log(persson1.name);
+
+// Making methods
+
+const user = {
+
+    name: 'Pedro',
+    surname: 'Alfredo',
+    age: 85,
+
+    speak() {
+        console.log(`${this.name} ${this.surname} is saying hi!`)
+    }
+}
+
+user.speak();
