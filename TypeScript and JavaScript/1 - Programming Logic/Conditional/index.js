ee017d17dd0627c6b669ac0f74aec0d64c557777
lfelipediniz <61145881+lfelipediniz@users.noreply.github.com>
2021-01-19 18:05:30 -0300

    Conditionals in JS

---
+++ TypeScript and JavaScript/1 - Programming Logic/Conditional/index.js
@@ -0,0 +1,18 @@
+const hour = 12;
+
+if(hour <= 11){
+
+    console.log('Good Morning!');
+}
+
+else if(hour >= 12 && hour <= 17){
+
+    console.log('Good Afternoon!');
+}
+
+else if (hour >= 18 && hour <= 23){
+
+    console.log('Good Night!');
+}
+
+else console.log('You put an invalid hour!');
