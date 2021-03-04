906993deffc5f915ad2ec112bfacd5c05fa4e313
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-04 18:56:12 -0300

    BMI update

--- TypeScript and JavaScript/1 - Programming Logic/2 - BMI exercise (improved code)/assets/js/main.js
+++ TypeScript and JavaScript/1 - Programming Logic/2 - BMI exercise (improved code)/assets/js/main.js
@@ -1,2 +1,12 @@
 // capture the form event
 
+const form = document.querySelector('#bmi-form');
+
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    console.log('Kapa');
+
+});
+
