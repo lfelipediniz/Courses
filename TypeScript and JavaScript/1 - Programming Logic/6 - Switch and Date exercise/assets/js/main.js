959fbff54e783fc2cb2a8e10a951f54cd0a0124f
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-21 15:29:02 -0300

    Switch and Date exercise

---
+++ TypeScript and JavaScript/1 - Programming Logic/6 - Switch and Date exercise/assets/js/main.js
@@ -0,0 +1,130 @@
+function getDayWeekText(dayWeek) {
+
+    let dayWeekText;
+
+    switch (dayWeek) {
+
+        case 0:
+            dayWeekText = 'Sunday';
+            return dayWeekText;
+
+        case 1:
+            dayWeekText = 'Monday';
+            return dayWeekText;
+
+        case 2:
+            dayWeekText = 'Tuesday';
+            return dayWeekText;
+
+        case 3:
+            dayWeekText = 'Wednesday';
+            return dayWeekText;
+
+        case 4:
+            dayWeekText = 'Thursday';
+            return dayWeekText;
+
+        case 5:
+            dayWeekText = 'Friday';
+            return dayWeekText;
+
+        case 6:
+            dayWeekText = 'Saturday';
+            return dayWeekText;
+    }
+}
+
+function getMonthText(month) {
+
+    let monthText;
+
+    switch (month) {
+
+        case 0:
+            monthText = 'January';
+            return monthText;
+
+        case 1:
+            monthText = 'February';
+            return monthText;
+
+        case 2:
+            monthText = 'March';
+            return monthText;
+
+        case 3:
+            monthText = 'April';
+            return monthText;
+
+        case 4:
+            monthText = 'May';
+            return monthTextt;
+
+        case 5:
+            monthText = 'June';
+            return monthText;
+
+        case 6:
+            monthText = 'July';
+            return monthText;
+
+        case 7:
+            monthText = 'August';
+            return monthText;
+
+        case 8:
+            monthText = 'September';
+            return monthText;
+
+        case 9:
+            monthText = 'October';
+            return monthText;
+
+        case 10:
+            monthText = 'November ';
+            return monthText;
+
+        case 11:
+            monthText = 'December';
+            return monthText;
+    }
+}
+
+function applyingZero (num) {
+
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formatDate(date) {
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    
+    const hour = date.getHours();
+    const min = date.getMinutes();
+
+    const monthText = getMonthText(month);
+
+    return `${monthText} ${day}, ${year} at ${hour}:${min}`
+
+}
+
+function finalProcessDate(){
+
+    const date = new Date();
+    const yourDate = formatDate(date);
+
+    let dayWeek = date.getDay();
+
+    const dayWeekText = getDayWeekText(dayWeek);
+
+    let finalDate =  `${dayWeekText}, ${yourDate}`;
+
+    const result = document.querySelector('#result');
+
+    return result.innerHTML = `${finalDate}`;
+}
+
+console.log(finalProcessDate());
+
