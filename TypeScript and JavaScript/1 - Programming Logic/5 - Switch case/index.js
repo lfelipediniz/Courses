1c9978c921df6b7a99a5885fdf60502d47fb1ef9
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-21 11:56:26 -0300

    Switch case

---
+++ TypeScript and JavaScript/1 - Programming Logic/5 - Switch case/index.js
@@ -0,0 +1,41 @@
+function getDayWeekText(dayWeek) {
+
+    let dayWeekText;
+
+    switch (dayWeek) {
+
+        case 0:
+            dayWeekText = 'Sunday';
+            return dayWeekText;
+
+        case 1:
+            dayWeekText = 'Monday';
+            return dayWeekText;
+
+        case 2:
+            dayWeekText = 'Tuesday';
+            return dayWeekText;
+
+        case 3:
+            dayWeekText = 'Wednesday';
+            return dayWeekText;
+
+        case 4:
+            dayWeekText = 'Thursday';
+            return dayWeekText;
+
+        case 5:
+            dayWeekText = 'Friday';
+            return dayWeekText;
+
+        case 6:
+            dayWeekText = 'Saturday';
+            return dayWeekText;
+    }
+}
+
+const date = new Date('2000-03-26 00:00:00');
+let dayWeek = date.getDay();
+
+const dayWeekText = getDayWeekText(dayWeek);
+console.log(dayWeek, dayWeekText);
