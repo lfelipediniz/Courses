00620841c13715a86666ed45b0f162bc7ea62172
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-20 23:15:25 -0300

    Ternary operation

---
+++ TypeScript and JavaScript/1 - Programming Logic/3 - Ternary operation/index.js
@@ -0,0 +1,11 @@
+// (condicion) ? 'True Value' : 'False Value';
+
+const userScore = 500;
+
+const userLevel = userScore >= 1000 ? 'Vip User' : 'Normal User';
+
+const userColor = null;
+const defaultColor = userColor || 'Black';
+
+console.log(userLevel);
+
