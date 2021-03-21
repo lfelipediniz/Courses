f26cdf8fb8ee8027adc9082aa4585874d0cc2610
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-20 22:15:12 -0300

    BMI rewriten

--- TypeScript and JavaScript/1 - Programming Logic/2 - BMI exercise (improved code)/assets/js/main.js
+++ TypeScript and JavaScript/1 - Programming Logic/2 - BMI exercise (improved code)/assets/js/main.js
@@ -1,5 +1,4 @@
 // capture the form event
-
 const form = document.querySelector('#bmi-form');
 
 form.addEventListener('submit', function (e) {
@@ -15,10 +14,41 @@ form.addEventListener('submit', function (e) {
     if (!weight || !height) { setResult('We detected an invalid value!', false); return; }
 
     const bMI = getBMI(weight, height);
+
+    const BMILevel = getBMILevel(bMI);
+
+    console.log(bMI, BMILevel);
 });
 
-// function with the specialty of creating parameters
+// Calculating BMI
+function getBMI (weight, height) {
 
+    const bMI = weight / height ** 2;
+    return bMI.toFixed(2);
+}
+
+// BMI Classified
+function getBMILevel(bMI) {
+
+    const level = [
+        
+        'Under weight',
+        'Normal weight',
+        'Overweight',
+        'Obesity grade one',
+        'Obesity grade two',
+        'Obesity grade three'
+    ];
+
+    if (bMI >= 39.9) return level[5];
+    if (bMI >= 34.9) return level[4];
+    if (bMI >= 29.9) return level[3];
+    if (bMI >= 24.9) return level[2];
+    if (bMI >= 18.5) return level[1];
+    if (bMI <  18.5) return level[0];
+}
+
+// function with the specialty of creating parameters
 function createP () {
 
     const p = document.createElement('p');
