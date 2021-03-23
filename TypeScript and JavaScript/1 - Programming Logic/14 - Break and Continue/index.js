400ce969687abdad2bb54bfe995a7b0839cd11e5
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-23 09:34:00 -0300

    Break and Continue

---
+++ TypeScript and JavaScript/1 - Programming Logic/14 - Break and Continue/index.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i of numbers){
+
+    if (i === 2) continue; // skipping number 2
+
+    console.log(i);
+
+    if (i === 7 ) break; // Leaving the loop, after finding the 7
+}
+
+
