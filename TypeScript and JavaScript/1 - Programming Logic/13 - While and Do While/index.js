260d6f4d14250b7b99f6c12204270f6658c7e48b
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-23 09:16:53 -0300

    While and Do While

---
+++ TypeScript and JavaScript/1 - Programming Logic/13 - While and Do While/index.js
@@ -0,0 +1,28 @@
+
+function random(min, max) {
+
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand = random(min, max);
+
+// while
+
+while (rand !== 10){
+
+    rand = random(min, max)
+    console.log(rand);
+}
+
+console.log('--------------------------')
+
+// do while
+
+do {
+
+    console.log(rand);
+
+} while(rand != 10);
