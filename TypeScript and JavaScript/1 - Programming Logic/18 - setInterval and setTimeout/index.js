94fcc0620406e4e6198b44e8b3772e3042788f41
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-25 15:16:39 -0300

    setInterval and setTimeout

---
+++ TypeScript and JavaScript/1 - Programming Logic/18 - setInterval and setTimeout/index.js
@@ -0,0 +1,25 @@
+function showHour() {
+
+    let date = new Date();
+
+    return date.toLocaleTimeString('pt-BR', {
+
+        hour12: false
+    });
+}
+
+// configure a time interval for the execution of a function
+
+const timer = setInterval(function() { 
+
+    console.log(showHour());
+
+}, 1000);
+
+// determines the amount of time the function will last
+
+setTimeout(function() {
+
+    clearInterval(timer);
+}, 10000)
+
