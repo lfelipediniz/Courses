b2e5595efaa0902fd30da07432dc53e21a95b1a2
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-26 08:53:37 -0300

    Creating a Timer

--- TypeScript and JavaScript/1 - Programming Logic/19 - Creating a Timer with setInterval/assets/js/main.js
+++ TypeScript and JavaScript/1 - Programming Logic/19 - Creating a Timer with setInterval/assets/js/main.js
@@ -1,46 +1,62 @@
+function clock() {
 
-function getTimeFromSeconds(second) {
+    function getTimeFromSeconds(second) {
 
-    const date = new Date(second * 1000);
+        const date = new Date(second * 1000);
 
-    return date.toLocaleTimeString('pt-BR', {
+        return date.toLocaleTimeString('pt-BR', {
 
-        hour12: false,
-        timeZone: 'UTC'
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+
+    const clock = document.querySelector('.timer');
+
+    let seconds = 0;
+    let timer;
+
+    // start clock
+
+    function timerStarter() {
+
+        timer = setInterval(function () {
+
+            seconds++;
+
+            clock.innerHTML = getTimeFromSeconds(seconds);
+
+        }, 1000)
+    };
+
+    // capture browser events
+
+    document.addEventListener('click', function (e) {
+
+        const element = e.target;
+
+        if (element.classList.contains('start')) {
+
+            clock.classList.remove('paused');
+
+            clearInterval(timer);
+            timerStarter();
+        }
+
+        if (element.classList.contains('pause')) {
+
+            clearInterval(timer);
+            clock.classList.add('paused'); // Add a css property, for the timer to change color
+        }
+
+        if (element.classList.contains('reset')) {
+
+            clearInterval(timer);
+            clock.innerHTML = '00:00:00';
+            seconds = 0;
+            clock.classList.remove('paused');
+        }
     });
 }
 
-console.log(getTimeFromSeconds(10));
-
-const clock = document.querySelector('.timer');
-const start = document.querySelector('.start')
-const pause = document.querySelector('.pause');
-const reset = document.querySelector('.reset');
-
-let seconds = 0;
-
-// start clock
-
-function timerStarter() {
-
-    const timer = setInterval(function () {
-
-        seconds++;
-
-        clock.innerHTML = getTimeFromSeconds(seconds);
-
-    }, 1000)
-};
-
-start.addEventListener('click', function (event) {
-
-    timerStarter();
-});
-
-pause.addEventListener('click', function (event) {
-
-});
-
-reset.addEventListener('click', function (event) {
-
-});
+clock();
