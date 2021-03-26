e192aab74f15777cf1165c3ff5666bddcd877551
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-03-26 09:56:21 -0300

    Task List

--- TypeScript and JavaScript/1 - Programming Logic/20 - Task List/assets/js/main.js
+++ TypeScript and JavaScript/1 - Programming Logic/20 - Task List/assets/js/main.js
@@ -1,1 +1,8 @@
  
+ // importing html parameters, as variables
+
+ const inputTask = document.querySelector('.task-input');
+ const btnTask = document.querySelector('.btn-task');
+ const tasks = document.querySelector('.tasks');
+
+ 
