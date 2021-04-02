67f671bfa4da2022751d2a87ba7affd7a598d5f8
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-02 10:50:45 -0300

    Second day

---
+++ NLW4/reactTrack/moveit-next/src/components/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallenges.module.css';
+
+export function CompletedClallenges() {
+
+    return (
+
+        <div className= {styles.completedChallengesContainer}>
+            <span>Complete Challenges</span>
+            <span>5</span>
+        </div>
+    )
+}
---
+++ NLW4/reactTrack/moveit-next/src/pages/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { CompletedClallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+   
+    <div className = {styles.container}>
+
+      <Head>
+        <title>Home | moveit</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <section>
+
+        <div>
+
+          <Profile />
+          <CompletedClallenges />
+          <Countdown />
+
+        </div>
+
+        <div>
+
+        </div>
+      </section>
+
+    </div>
+
+  )
+}
