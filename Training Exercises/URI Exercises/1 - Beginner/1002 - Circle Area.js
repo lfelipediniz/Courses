93b2fc8cc606a5233d53ba6e7d8fa10ea307b4c7
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-04 00:13:43 -0300

    1002

--- Training Exercises/URI Exercises/1 - Beginner/1002 - Circle Area.js
+++ Training Exercises/URI Exercises/1 - Beginner/1002 - Circle Area.js
@@ -8,6 +8,7 @@ const consulta = readline.createInterface({
 });
 
 consulta.on("line", (radius) => {
+    
     let raio = Number(radius) * Number(radius);
     let area = raio * 3.14159;
     console.log("A=" + area.toFixed(4));
