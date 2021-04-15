9c42efe83803cb544cdf6c9ca9f6fbdc86884b69
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-15 18:43:06 -0300

    Exporting multiple functions

---
+++ NodeJS/00 - Introduction to Node/01 - Exporting multiple functions/app.js
@@ -0,0 +1,7 @@
+let calculator = require("./calculator"); /* importing the sum function, and storing is
+variable */
+
+
+console.log(
+calculator.sum(23, 34), calculator.div(4, 2), calculator.mul(32, 2),
+calculator.sub(10, 5));
---
+++ NodeJS/00 - Introduction to Node/01 - Exporting multiple functions/calculator.js
@@ -0,0 +1,10 @@
+function sum(a, b) { return a + b; }
+
+function mul(a, b) { return a * b; }
+
+function  div(a, b) { return a / b; }
+
+function sub(a, b) { return a - b; }
+
+// exporting more than one function
+module.exports = { sum, mul, div, sub }
