34500726dd5ee3f73cb7d5e7e1a4caadc9caa54f
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-19 22:50:19 -0300

    State with Arrays and Objects

--- ReactJS/00 - Basic React/01 - State with Arrays and Objects/app/src/App.js
+++ ReactJS/00 - Basic React/01 - State with Arrays and Objects/app/src/App.js
@@ -1,25 +1,24 @@
-import "./App.css";
+import { render } from "@testing-library/react";
 import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
     posts: [
       {
         id: 1,
-        title: "Title 1",
-        body: "body 1",
+        title: "i am greeting you for the 1st time",
+        body: "Body 1",
       },
-
       {
         id: 2,
-        title: "Title 2",
-        body: "body 2",
+        title: "i am greeting you for the 2st time",
+        body: "Body 2",
       },
-
       {
         id: 3,
-        title: "Title 3",
-        body: "body 3",
+        title: "i am greeting you for the 3st time",
+        body: "Body 3",
       },
     ],
   };
@@ -32,6 +31,7 @@ class App extends Component {
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
+            {/* react needs a key to more efficiently optimize the code */}
             <p>{post.body}</p>
           </div>
         ))}
@@ -39,4 +39,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
