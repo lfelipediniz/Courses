5443fbc424f70fe4374b6250a735b020dbddd072
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-28 16:39:09 -0300

    Using nodemon

--- NodeJS/02 - Express Fundamentals/02 - Parameters/app/index.js
+++ NodeJS/02 - Express Fundamentals/02 - Parameters/app/index.js
@@ -1,18 +1,16 @@
-const express = require('express'); // importing express
+const express = require("express"); // importing express
 const app = express(); // starting express
 
-app.get("/", function(req, res) {
-    res.send("Welcome to my Website!");
-
+app.get("/", function (req, res) {
+  res.send("Welcome to my Website!");
 }); // defining initial application route
 
-app.get("/blog", function(req, res) {
-    res.send("Welcome to my Blog");
+app.get("/blog", function (req, res) {
+  res.send("Welcome to my Blog");
+});
 
-}); 
-
-app.listen(4001, function(error){
-
- error ? console.log("Oh no! An error has occurred!") : console.log("Server started successfully"); 
-})
-
+app.listen(4001, function (error) {
+  error
+    ? console.log("Oh no! An error has occurred!")
+    : console.log("Server started successfully");
+});
