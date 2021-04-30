3260745da1bfd923cb4ebe1d8ec4e0bdbbd46f81
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-04-29 23:54:37 -0300

    Data Fetching

--- ReactJS/00 - Basic React/03 - Data fetching/app/src/App.js
+++ ReactJS/00 - Basic React/03 - Data fetching/app/src/App.js
@@ -1,33 +1,49 @@
-import { render } from "@testing-library/react";
-import { Component } from "react";
-import "./App.css";
+import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   state = {
-    counter: 0,
-
     posts: []
   };
 
-  componentDidMount() { 
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            {/* react needs a key to more efficiently optimize the code */}
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
-
 export default App;
