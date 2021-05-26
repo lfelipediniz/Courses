6cb35ae2e97a99045e695af900b5e5ef8f302a77
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-05-25 23:55:57 -0300

    Manipulating Prototypes

---
+++ TypeScript and JavaScript/04 - Objects and Prototype (Advanced)/05 - Manipulating prototypes/index.js
@@ -0,0 +1,29 @@
+function Product(name, price) {
+    this.name = name;
+    this.price - price;
+}
+
+Product.prototype.discount = function(percentage) {
+    this.price = this.price - (this.price * (percentage / 100));
+};
+
+Product.prototype.increase = function(percentage) {
+    this.price = this.price + (this.price * (percentage / 100));
+};
+
+const p1 = new Product('shirt', 50);
+
+// Literal
+const p2 = {
+    name: 'Cup',
+    price: 15
+};
+
+//Object.setPrototypeOf(p2, Product.prototype)
+
+p2.increase(10);
+
+console.log(p1);
+console.log(p2);
+
+
