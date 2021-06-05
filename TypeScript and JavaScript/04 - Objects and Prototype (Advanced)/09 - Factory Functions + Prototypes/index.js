9f20b54a556ffd23d587e5a457ee07e429608d1e
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2021-06-04 23:23:06 -0300

    Factory Functions and Prototypes

---
+++ TypeScript and JavaScript/04 - Objects and Prototype (Advanced)/09 - Factory Functions + Prototypes/index.js
@@ -0,0 +1,31 @@
+const speak = {
+    speak() {
+      console.log(`${this.name} is talking.`);
+    },
+  };
+  
+  const eat = {
+    eat() {
+      console.log(`${this.name} is eating.`);
+    },
+  };
+  
+  const drink = {
+    drink() {
+      console.log(`${this.name} is drinking.`);
+    },
+  };
+  
+  const prototypePerson = Object.assign({}, speak, eat, drink);
+  
+  function createPerson(name, surname) {
+    return Object.create(prototypePerson, {
+      name: { value: name },
+      surname: { value: surname }
+    });
+  }
+  
+  const p1 = createPerson('Luiz', 'Otávio');
+  const p2 = createPerson('Maria', 'Antonieta');
+  console.log(p1, p2);
+  
